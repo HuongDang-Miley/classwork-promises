773c38ca29470548f7838710843177c088b0271c
Huong Dang <miley.dawn@codeimmersives.com>
2020-09-21 15:44:07 -0400

    finish classwork

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // 1.
-// Create a promise called myFirstPromise
 
 myFirstPromise = new Promise((resolve, reject) => {
   // Create a boolean variable check and set it to true
   check = true;
   // Create a variable rand and it should calculate a random number between 1 and 10
-  rand = Math.random() * 10
+  rand = Math.floor(Math.random() * 10 + 1)
   // If the check boolean is true it should resolve with the result of a random number between one and ten
   if (check) {
     setTimeout(() => {
@@ -30,6 +29,33 @@ myFirstPromise
   })
 
 
+// // JD solution
+
+// let myFirstPromise = new Promise((resolve, reject) => {
+//   let check = false
+//   let rand = Math.floor(Math.random() * 10 + 1)
+//   setTimeout(() => {
+//     if (!check) {
+//       reject('Cannot computer random number')
+//     }
+//     else {
+//       resolve(rand)
+//     }
+//   }, 2000)
+// });
+
+// myFirstPromise
+//   .then((randomNumber) => {
+//     console.log(`I have my random number ${data} and I will multiply it by 5`)
+//     let newNumber = randomNumber * 5
+//     return newNumber
+//   })
+//   .then((nextNumber) => {
+//     console.log(`Here is the result of my random number multiplied by 5: ${nextNumber * 5}`)
+//   })
+//   .catch((err) => { console.log(err) })
+
+
 
 
 // 2.
@@ -51,25 +77,27 @@ let data = [
   { firstName: 'Sandra', lastName: 'Mathers' },
 ];
 
-// const test = data.forEach((obj) => {console.log( `Hello ${obj.firstName} ${obj.lastName}`)})
-
-// test
-const getDataPromise = function (data) {
-  return new Promise((reject) => {
+const getDataPromise = function (arr) {
+  return new Promise((resolve, reject) => {
     const error = false
-    if (error) {
-      reject('Something went wrong')
-    }
-    else {
-      setTimeout(() => {
-        data.forEach((obj) => { console.log(`Hello ${obj.firstName} ${obj.lastName}`) })
-      }, 2000)
-    }
+    setTimeout(() => {
+      if (error) {
+        reject('Something went wrong')
+      }
+      else {
+        resolve(arr)
+      }
+    }, 4000)
   })
-
 }
 
 getDataPromise(data)
+  .then((data) => {
+    for (const obj of data) {
+      console.log(`Hello ${obj.firstName} ${obj.lastName}`)
+    }
+  })
   .catch((err) => {
     console.log(`${err}: There's an error`)
   })
+
